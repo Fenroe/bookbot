ec7c5014a60e3744d18ecfcb12a2008c1d412a0d
fenroe <scrambles9494@gmail.com>
2024-09-19 15:21:01 +0300

    Feat: refactored previous functions into more reusable components. Added function to count words in text

--- main.py
+++ main.py
@@ -1,11 +1,25 @@
-def read_frankenstein():
+def get_frankenstein_text():
     with open("./books/frankenstein.txt") as f:
         file_contents = f.read()
-        print(file_contents)
+        return file_contents
 
+def print_text(text):
+    print(text)
 
+def count_words(text):
+    try:
+        word_count = len(text.split())
+        return word_count
+    except Exception as e:
+        print("Error in the count_words function. See below")
+        print(e)
 
 def main():
-    read_frankenstein()
+    # Frankenstein
+    frankenstein_text = get_frankenstein_text()
+    print_text(frankenstein_text)
+    # newline
+    print("")
+    print(f"Total words in Frankenstein: {count_words(frankenstein_text)}")
 
 main()
